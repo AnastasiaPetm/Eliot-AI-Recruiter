7d1d00990f5fc8c26407ca2e8c009e3addb69b91
AnastasiaPetm <anastasia.petmeza@yahoo.com>
2025-07-11 11:48:35 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Welcome to Eliot</h1>
+      <p>This is a simple demo app deployed with Vercel.</p>
+    </div>
+  );
+}
+
+export default App;
